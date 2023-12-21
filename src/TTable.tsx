87ac115e1b52bd900ef4317af38e9662225e4ddf
weizhaoma <weizhao.ma@qsan.com>
2023-12-21 10:42:50 +0800

    update

--- src/TTable.tsx
+++ src/TTable.tsx
@@ -89,7 +89,7 @@
 // };
 
 import React from "react";
-import { Table } from "antd";
+import Table from "antd/es/table";
 
 // import { ThemeContext, AddImportantToStyles } from "./TThemeProvider";
 // import MColors from "./MColors.json";
