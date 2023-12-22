da228a99c94b5a8feed57eca6a5623cd23b2d667
weizhaoma <weizhao.ma@qsan.com>
2023-12-22 14:19:43 +0800

    update

--- index.d.ts
+++ index.d.ts
@@ -1,70 +1,63 @@
 declare module "design-ui-test" {
-  import React from "react";
-  import { TableProps } from "antd";
-  import styled, { Interpolation } from "@emotion/styled";
-  import { Theme } from "@emotion/react";
-  import { SwitchProps } from "antd";
-  import { ReactNode } from "react";
+  // import React from "react";
+  // import { TableProps } from "antd";
+  // import styled, { Interpolation } from "@emotion/styled";
+  // import { Theme } from "@emotion/react";
+  // import { SwitchProps } from "antd";
+  // import { ReactNode } from "react";
+  // export interface TTableColor {
+  //   headerColor?: string;
+  //   rowColor?: string;
+  //   hoverRowColor?: string;
+  //   textColor?: string;
+  // }
+  // export interface TTableProps {
+  //   customStyle?: Interpolation<Theme>;
+  //   tableColor?: TTableColor;
+  // }
+  // export interface TSwitchProps {
+  //   customStyle?: Interpolation<Theme>;
+  // }
+  // export interface TButtonProps {
+  //   onClick?: React.MouseEventHandler<HTMLElement> | undefined;
+  //   qType?: string;
+  //   qSize?: string;
+  //   disabled?: boolean;
+  //   loading?: boolean;
+  //   children?: ReactNode;
+  //   className?: string;
+  //   style?: React.CSSProperties;
+  //   customStyle?: Interpolation<Theme>;
+  // }
+  // export interface TThemeProviderProps {
+  //   colorScheme?: {
+  //     text?: string;
+  //     main?: string;
+  //     secondary?: string;
+  //     accent?: string;
+  //   };
+  //   fontSize?: {
+  //     t1?: number;
+  //     t2?: number;
+  //     t3?: number;
+  //     t4?: number;
+  //     t5?: number;
+  //   };
+  //   size?: "small" | "middle" | "large";
+  //   locale?: string;
+  //   localeBundle?: { lng: string; ns: "translation"; resources: any }[];
+  //   children: any;
+  //   fontFamily?: string;
+  // }
+  // export interface TCowsayProps {
+  //   content?: string;
+  // }
+  // const TTable: React.FC<TTableProps & TableProps<any>>;
+  // const TSwitch: React.FC<TSwitchProps & SwitchProps>;
+  // const TButton: React.FC<TButtonProps>;
+  // const TThemeProvider: React.FC<TThemeProviderProps>;
+  // const TCowsay: React.FC<TCowsayProps>;
+  // export { TTable, TSwitch, TButton, TThemeProvider, TCowsay };
 
-  export interface TTableColor {
-    headerColor?: string;
-    rowColor?: string;
-    hoverRowColor?: string;
-    textColor?: string;
-  }
-
-  export interface TTableProps {
-    customStyle?: Interpolation<Theme>;
-    tableColor?: TTableColor;
-  }
-
-  export interface TSwitchProps {
-    customStyle?: Interpolation<Theme>;
-  }
-
-  export interface TButtonProps {
-    onClick?: React.MouseEventHandler<HTMLElement> | undefined;
-    qType?: string;
-    qSize?: string;
-
-    disabled?: boolean;
-    loading?: boolean;
-    children?: ReactNode;
-    className?: string;
-    style?: React.CSSProperties;
-    customStyle?: Interpolation<Theme>;
-  }
-
-  export interface TThemeProviderProps {
-    colorScheme?: {
-      text?: string;
-      main?: string;
-      secondary?: string;
-      accent?: string;
-    };
-    fontSize?: {
-      t1?: number;
-      t2?: number;
-      t3?: number;
-      t4?: number;
-      t5?: number;
-    };
-    size?: "small" | "middle" | "large";
-    locale?: string;
-    localeBundle?: { lng: string; ns: "translation"; resources: any }[];
-    children: any;
-    fontFamily?: string;
-  }
-
-  export interface TCowsayProps {
-    content?: string;
-  }
-
-  const TTable: React.FC<TTableProps & TableProps<any>>;
-  const TSwitch: React.FC<TSwitchProps & SwitchProps>;
-  const TButton: React.FC<TButtonProps>;
-  const TThemeProvider: React.FC<TThemeProviderProps>;
-  const TCowsay: React.FC<TCowsayProps>;
-
-  export { TTable, TSwitch, TButton, TThemeProvider, TCowsay };
+  export * from "./dist";
 }
