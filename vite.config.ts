048ad460f44adccc1fcd820c78b29a5b46be60cb
weizhaoma <weizhao.ma@qsan.com>
2023-12-22 11:04:20 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,10 @@ import react from "@vitejs/plugin-react";
 import path from "node:path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
-import { splitVendorChunkPlugin } from "vite";
 
 export default defineConfig({
   plugins: [
     react(),
-    splitVendorChunkPlugin(),
     dts({
       insertTypesEntry: true,
     }),
@@ -15,8 +13,8 @@ export default defineConfig({
   build: {
     minify: false,
     lib: {
+      name: "design-ui-test",
       entry: path.resolve(__dirname, "src"),
-      name: "MyLib",
       formats: ["es"],
       fileName: (format, entryName) => `${entryName}.js`,
     },
@@ -30,21 +28,6 @@ export default defineConfig({
           "@emotion/react": "emotionReact",
           "@emotion/styled": "emotionStyled,",
         },
-        inlineDynamicImports: false,
-
-        // manualChunks: (id) => {
-        //   // 可以把 id 印出來看，會更清楚知道他的內容
-        //   // console.log('id', id)
-        //   if (id.includes("node_modules")) {
-        //     const arr = id.toString().split("node_modules/")[1].split("/");
-        //     switch (arr[0]) {
-        //       case "element-plus":
-        //         return;
-        //       default:
-        //         return arr[0];
-        //     }
-        //   }
-        // },
       },
     },
   },
