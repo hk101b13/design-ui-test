aa09da99541eb22d0bc61b9ef64b1dd5336efbc8
weizhaoma <weizhao.ma@qsan.com>
2023-12-22 15:09:05 +0800

    update

--- index.d.ts
+++ index.d.ts
@@ -1,62 +1,55 @@
 declare module "design-ui-test" {
   import { TableProps } from "antd";
-  // import styled, { Interpolation } from "@emotion/styled";
-  // import { Theme } from "@emotion/react";
   import { SwitchProps } from "antd";
-  import { TButtonProps } from "./src/TButton";
-  import { TSwitchProps } from "@/TSwitch";
-  import { TTableProps } from "@/TTable";
-  import { TThemeProviderProps } from "@/TThemeProvider";
-  import { TCowsayProps } from "@/TCowsay";
 
-  // export interface TTableColor {
-  //   headerColor?: string;
-  //   rowColor?: string;
-  //   hoverRowColor?: string;
-  //   textColor?: string;
-  // }
-  // export interface TTableProps {
-  //   customStyle?: Interpolation<Theme>;
-  //   tableColor?: TTableColor;
-  // }
-  // export interface TSwitchProps {
-  //   customStyle?: Interpolation<Theme>;
-  // }
-  // export interface TButtonProps {
-  //   onClick?: React.MouseEventHandler<HTMLElement> | undefined;
-  //   qtype?: "info" | "success" | "warning" | "error";
-  //   qsize?: "large" | "middle" | "small";
+  export interface TTableColor {
+    headerColor?: string;
+    rowColor?: string;
+    hoverRowColor?: string;
+    textColor?: string;
+  }
+  export interface TTableProps {
+    customStyle?: Interpolation<Theme>;
+    tableColor?: TTableColor;
+  }
+  export interface TSwitchProps {
+    customStyle?: Interpolation<Theme>;
+  }
+  export interface TButtonProps {
+    onClick?: React.MouseEventHandler<HTMLElement> | undefined;
+    qtype?: "info" | "success" | "warning" | "error";
+    qsize?: "large" | "middle" | "small";
 
-  //   disabled?: boolean;
-  //   loading?: boolean;
-  //   children?: ReactNode;
-  //   className?: string;
-  //   style?: React.CSSProperties;
-  //   customstyle?: Interpolation<Theme>;
-  // }
-  // export interface TThemeProviderProps {
-  //   colorScheme?: {
-  //     text?: string;
-  //     main?: string;
-  //     secondary?: string;
-  //     accent?: string;
-  //   };
-  //   fontSize?: {
-  //     t1?: number;
-  //     t2?: number;
-  //     t3?: number;
-  //     t4?: number;
-  //     t5?: number;
-  //   };
-  //   size?: "small" | "middle" | "large";
-  //   locale?: string;
-  //   localeBundle?: { lng: string; ns: "translation"; resources: any }[];
-  //   children: any;
-  //   fontFamily?: string;
-  // }
-  // export interface TCowsayProps {
-  //   content?: string;
-  // }
+    disabled?: boolean;
+    loading?: boolean;
+    children?: ReactNode;
+    className?: string;
+    style?: React.CSSProperties;
+    customstyle?: Interpolation<Theme>;
+  }
+  export interface TThemeProviderProps {
+    colorScheme?: {
+      text?: string;
+      main?: string;
+      secondary?: string;
+      accent?: string;
+    };
+    fontSize?: {
+      t1?: number;
+      t2?: number;
+      t3?: number;
+      t4?: number;
+      t5?: number;
+    };
+    size?: "small" | "middle" | "large";
+    locale?: string;
+    localeBundle?: { lng: string; ns: "translation"; resources: any }[];
+    children: any;
+    fontFamily?: string;
+  }
+  export interface TCowsayProps {
+    content?: string;
+  }
   const TTransfer: React.FC;
   const TTable: React.FC<TTableProps & TableProps<any>>;
   const TSwitch: React.FC<TSwitchProps & SwitchProps>;
--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
       insertTypesEntry: true,
     }),
   ],
+  server: {
+    host: "0.0.0.0",
+  },
   build: {
     minify: false,
     lib: {
